26c33ff2b1c546cd27aaa8d88fab4acddcaa58c7
sheryar kayani <sheryarali2010@gmail.com>
2024-10-21 14:41:50 +0500

    v1

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {}
-  },
-  server: {
-    port: 3000 // You can change this to any available port
-  },
-  build: {
-    outDir: 'dist'
-  }
+  base: '/',
 })
